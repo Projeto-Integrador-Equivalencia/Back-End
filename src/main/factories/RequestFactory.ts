6dac11f9e79166c0ef2eab57b25fe8ef7b36064b
Gustavo <gustavocesargevigi@gmail.com>
2026-05-04 12:47:00 -0300

    Feat: Envio de email como notificacao para a acao de criacao de solicitacao

--- src/main/factories/RequestFactory.ts
+++ src/main/factories/RequestFactory.ts
@@ -1,6 +1,8 @@
 import { RequestController } from '../../controllers/RequestController';
 import { SequelizeRequestRepository } from '../../infrastructure/database/sequelize/repositories/SequelizeRequestRepository';
+import { SequelizeStudentRepository } from '../../infrastructure/database/sequelize/repositories/SequelizeStudentRepository';
 import { ActionLogRepository } from '../../infrastructure/database/mongoose/repositories/ActionLogRepository';
+import { EtheralMailProvider } from '../../infrastructure/mail/EtheralMailProvider';
 
 import { AddObservationService } from '../../services/request/AddObservationService';
 import { CancelRequestService } from '../../services/request/CancelRequestService';
@@ -17,6 +19,8 @@ export class RequestFactory {
   static create(): RequestController {
     const requestRepository = new SequelizeRequestRepository();
     const logRepository = new ActionLogRepository();
+    const mailProvider = new EtheralMailProvider();
+    const studentRepository = new SequelizeStudentRepository();
 
     const addObservationService = new AddObservationService(
       requestRepository,
@@ -24,7 +28,7 @@ export class RequestFactory {
     );
     const cancelRequestService = new CancelRequestService(requestRepository);
     const checkDuplicityService = new CheckDuplicityService(requestRepository);
-    const createRequestService = new CreateRequestService(requestRepository, logRepository);
+    const createRequestService = new CreateRequestService(requestRepository, logRepository, mailProvider, studentRepository);
     const findAdvisorCourseService = new FindAdvisorCourseService(requestRepository);
     const findByIdService = new FindByIdService(requestRepository, logRepository);
     const findByProtocolService = new FindByProtocolService(requestRepository);
